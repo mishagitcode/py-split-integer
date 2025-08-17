1109df20552fe50f532c6cc490782e0462ff8fd3
Misha Zubritsky <zubritsky.misha@gmail.com>
2025-08-17 18:48:22 +0300

    implemented py-split-integer

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,20 +2,30 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    pass
+    assert sum(split_integer(6, 2)) == 6
+    assert sum(split_integer(17, 4)) == 17
+    assert sum(split_integer(32, 6)) == 32
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    pass
+    assert split_integer(8, 4) == [2, 2, 2, 2]
+    assert split_integer(12, 6) == [2, 2, 2, 2, 2, 2]
+    assert split_integer(9, 3) == [3, 3, 3]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    pass
+    assert split_integer(8, 1) == [8]
+    assert split_integer(190, 1) == [190]
+    assert split_integer(0, 1) == [0]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    pass
+    result = split_integer(17, 4)
+    assert result == sorted(result)
+    result = split_integer(32, 6)
+    assert result == sorted(result)
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    pass
+    assert split_integer(3, 5) == [0, 0, 1, 1, 1]
+    assert split_integer(1, 4) == [0, 0, 0, 1]
